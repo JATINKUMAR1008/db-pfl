ae9901fe365e797e484ce98a5ef6944ef6b85497
jatin <jatinkumar10082003@gmail.com>
2024-02-24 18:52:15 +0530

    fix error build

--- frontend-server/src/app/page.tsx
+++ frontend-server/src/app/page.tsx
@@ -1,9 +1,10 @@
 "use client";
-import Image from "next/image";
 import { useRouter } from "next/navigation";
-
+import { useEffect } from "react";
 export default function Home() {
   const router = useRouter();
-  router.push("/home");
+  useEffect(() => {
+    router.push("/home");
+  }, []);
   return <div>Page</div>;
 }
