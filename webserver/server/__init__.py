fa452460c4488f4009ae591e6435bd529c8e63f3
jatin <jatinkumar10082003@gmail.com>
2024-02-26 15:49:39 +0530

    remove server

--- webserver/server/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from dotenv import load_dotenv
-import os
-load_dotenv()
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
-print(os.getenv('DATABASE_URI'))
-from server import routes
